1169821275ca1610bcaecb0a57717024ca9761d8
magistrigusti <47912728+magistrigusti@users.noreply.github.com>
2025-04-29 09:39:27 +0300

    0.1.04

---
+++ controllers/user-controller.js
@@ -0,0 +1,20 @@
+
+const UserController = {
+  register: async (req, res) => {
+    res.send('register')
+  },
+  login: async (req, res) => {
+    res.send('login')
+  },
+  getUserById: async (req, res) => {
+    res.send('getUserById')
+  },
+  updateUser: async (req, res) => {
+    res.send('updateUser')
+  },
+  currentUser: async (req, res) => {
+    res.send(updateUser)
+  },
+}
+
+module.exports = UserController;
