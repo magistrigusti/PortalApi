ecc09a926aa59a93e097536fbd108e62850e1b5a
magistrigusti <47912728+magistrigusti@users.noreply.github.com>
2025-05-02 17:43:09 +0300

    0.2.06

--- controllers/post-controller.js
+++ controllers/post-controller.js
@@ -2,13 +2,88 @@ const { prisma } = require('../prisma/prisma-client');
 
 const PostController = {
   createPost: async (req, res) => {
-    res.send('createPost');
+    const { content } = req.body;
+    const authorId = req.user.userId;
+
+    if (!content) {
+      return res.status(400).json({ error: "Все поля обязательны"});
+    }
+
+    try {
+      const post = await prisma.post.create({
+        data: {
+          content,
+          authorId
+        }
+      });
+
+      res.json(post);
+    } catch (error) {
+      console.error('Create post error', );
+
+      res.status(500).json({ error: 'Internal server error'});
+    }
   },
   getAllPosts: async (req, res) => {
-    res.send('getAllPosts');
+    const userId = req.user.userId;
+
+    try {
+      const posts = await prisma.post.findMany({
+        include: {
+          likes: true,
+          author: true, 
+          comments: true,
+        },
+        orderBy: {
+          createdAt: 'desc'
+        }
+      });
+
+      const postWithLikeInfo = posts.map(post => ({
+        ...post,
+        likedByUser: post.likes.some(like => like.userId === userId)
+      }));
+
+      res.json(postWithLikeInfo)
+    } catch (error) {
+      console.error('get all post error', error);
+
+      res.status(500).json({ error: 'Internal server error'});
+    }
   },
   getPostById: async (req, res) => {
-    res.send('getPostById');
+    const { id } = req.params;
+    const userId = req.user.userId;
+
+    try {
+      const post = await prisma.post.findUnique({
+        where: { id },
+        include: {
+          comments: {
+            include: {
+              user: true 
+            }
+          },
+          likes: true,
+          author: true
+        }
+      });
+
+      if (!post) {
+        return res.status(404).json({ error: 'Пост не найден'});
+      }
+
+      const postWithLikeInfo = {
+        ...post,
+        likedByUser: post.likes.some(like => like.userId === userId)
+      }
+
+      res.json(postWithLikeInfo)
+    } catch ( error ) {
+      console.error("Get Post by Id error", error);
+
+      res.status(500).json({ error: 'Internal server error'});
+    }
   },
   deletePost: async (req, res) => {
     res.send('deletePost');
--- try.js
+++ try.js
@@ -1,170 +1,58 @@
-const bcrypt = require("bcryptjs");
-const path = require("path");
-const fs = require('fs');
-const jwt = require("jsonwebtoken");
-const { prisma } = require("../prisma/prisma-client");
-const Jdenticon = require('jdenticon');
+const { prisma } = require('../prisma/prisma-client');
 
-const UserController = {
-  register: async (req, res) => {
-    const { email, password, name } = req.body;
-    if (!email || !password || !name) {
-      return res.status(400).json({error: "All fields are required"});
+const PostController = {
+  createPost: async (req, res) => {
+    const { content } = req.body;
+    const authorId = req.user.userId;
+
+    if (!content) {
+      return res.status(400).json({ error: 'Все поля обязательны'});
     }
 
     try {
-      const existingUser = await prisma.user.findUnique({ where: { email }});
-      if (existingUser) {
-        return res.status(400).json({ error: "Пользователь уже существует"});
-      }
-
-      const hashedPassword = await bcrypt.hash(password, 10);
-      const png = Jdenticon.toPng(name, 200);
-      const avatarName = `${name}_${Date.now()}.png`;
-      const avatarPath = pathjoin(__dirname, '/../uploads', avatarName);
-      fs.writeFileSync(avatarPath, png);
-
-      const user = await prisma.user.create({
+      const post = await prismapost.create({
         data: {
-          email,
-          password: hashedPassword,
-          name,
-          avatarUrl: `/uploads/${avatarName}`,
-        },
+          content,
+          authorId
+        }
       });
 
-      res.json(user);
+      res.json(post);
     } catch (error) {
-      console. error("Error in register:", error);
-      res.status(500).json({error: "Internal server error"});
+      console.error('create post error');
+
+      res.status(500).json({ error: 'Internal server error'});
     }
   },
-  login: async (req, res) => {
-    const { email, password } = req.body;
-    if (!email || !password) {
-      return res.status(400).json({ error: "Все поля обязательны" });
-    }
-    try {
-      const user = await prisma.user.findUnique({ where: { email }});
-      if (!user) {
-        return res.status(400).json({ error: "Неверный логин или пароль"});
-      }
-
-      const valid = await bcrypt.compare(password, user.password);
-      if (!valid) {
-        return res.status(400).json({ error: "Неверный логин или пароль"});
-      }
-
-      const token =  jwt.sign({ userId: user.id }, process.env.SECRET_KET);
-
-      res.json({ token });
-    } catch (error) {
-      console.error("Error in login:", error);
-      res.status(500).json({ error: "Initial server error"});
-    }
-  },
-  getUserById: async (req, res) => {
-    const { id } = req.params;
-    const userId = req.user.UserId;
+  getAllPosts: async (req, res) => {
+    const userId = req.user.userId;
 
     try {
-      const user = await prisma.user.findUnique({
-        where: { id },
+      const posts = await prisma.post.findMany({
         include: {
-          followers: true,
-          following: true,
+          likes: true,
+          author: true,
+          comments: true,
         },
-      });
-
-      if (!user) {
-        return res.status(404).json({ error: "Пользователь не найден"});
-      }
-
-      const isFollowing = await prisma.follows.findFirs({
-        where: {
-          AND: [
-            { followerId: userId},
-            { followingId: id}
-          ]
+        orderBy: {
+          createdAt: 'desc'
         }
       });
 
-      res.json({...user, isFollowing: Boolean(isFollowing)})
+      const postWithLikeInfo = posts.map(post => ({
+        ...post,
+        likedBy
+      }))
     } catch (error) {
-      console.error('Get Current Error', error);
+      console.error('get all post error', error);
 
-      res.status(500).json({ error: "Internal server error"});
+      res.status(500).json({ error: 'Internal server error'});
     }
   },
-  updateUser: async (req, res) => {
-    const { id } = req.params;
-    const { email, name, dateOfBirth, bio, location } = req.body;
-    let filePath;
-
-    if (req.file && req.file.path) {
-      filePath = req.file.path;
-    }
-
-    if ( id !== req.user.userId) {
-      return res.status(403).json({ error: 'Нет доступа'});
-    }
-
-    try {
-      if (email) {
-        const existingUser = await prisma.user.findFirst({
-          where: { email: email }
-        });
-
-        if (exstingUser && existingUser.id !== id) {
-          return res.status(400).json({ error: "Почта уже используеться"});
-        }
-      }
-
-      const user = await prisma.user.update({
-        where: { id },
-        data: {
-          email: email || undefined,
-          name: name || undefined,
-          avatarUrl: filePath ? `/${filePath}` : undefined,
-          dateOfBirth: dateOfBirth || undefined,
-          bio: bio || undefined,
-          location: location || undefined 
-        }
-      });
-
-      res.json(user);
-    } catch (error) {
-      console.log("Update user error", error);
-      res.status(500).json({ error: "internal server error"});
-    }
+  getPostById: async (req, res) => {
+    res.send('getPostById');
   },
-  current: async (req, res) => {
-    try {
-      const user = await prisma.user.findUnique({
-        where: {
-          id: req.user.userId 
-        },
-        include: {
-          followers: {
-            include: {
-              follower: true
-            }
-          },
-          following: {
-            include: {
-              following: true
-            }
-          }
-        }
-      });
-
-
-    } catch (error) {
-      console.error('Get Current Error', error);
-
-      res.status(500).json({error: "Internal server error"});
-    }
-  },
+  deletePost: async (req, res) => {
+    res.send('deletePost');
+  }
 }
-
-module.export = UserController;
